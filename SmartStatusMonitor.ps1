997805cee6f57bda2cd0fe0ec5939f58ec6cd1a0
Joel Timothy Oh <joeltimothyoh@live.com>
2017-11-26 05:58:06 +0800

    Standardized format of report to support multiple outputs

--- SmartStatusMonitor.ps1
+++ SmartStatusMonitor.ps1
@@ -11,7 +11,7 @@ $Physical_Drives_Info = Get-WmiObject -Class Win32_DiskDrive
 $faulty_drives = $Physical_Drives_Info | Where-Object { $_.Status -ne 'OK'} | Measure-Object
 
 # Generate a table containing each physical drive with their respective capacities and SMART statuses
-$smart_status_report = $Physical_Drives_Info | 
+$smart_status_table = $Physical_Drives_Info | 
     Format-Table DeviceID, 
                  #Caption,          # Use Caption for drive model name 
                  @{ Name = "Size (GB)"; Expression = { [math]::Round($_.Size/1GB,1) } },
@@ -32,15 +32,19 @@ if ($faulty_drives.Count -gt 0) {
 }
 
 # Format body of report
+$smart_status_report = @()
+$smart_status_report += "Smart Status Check as of: $(Get-Date)"
+$smart_status_report += $smart_status_table
+
+# Print report the stdout
+Write-Output $title
+Write-Output $smart_status_report
+
+# Format body of email
 $body = @()
-$body += "<pre><p style='font-family: Courier New;'>"
-$body += "Smart Status Check as of: $(Get-Date)"
+$body += "<pre><p style='font-family: Courier New; font-size: 11px;'>"
 $body += $smart_status_report
 $body += "</p></pre>"
-
-# Print report to console
-$title + "`n"
-$body
 
 # Email the report
 Send-Mailmessage -to $email_to -subject $title -Body ( $body | Out-String ) -SmtpServer $smtp_server -from $smtp_email -Port $smtp_port -UseSsl -Credential $credential -BodyAsHtml
